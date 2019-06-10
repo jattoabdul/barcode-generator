9736592069197ae5b7814bc5b0dac01443363cea
jattoabdul <jattoade@gmail.com>
2019-06-10 18:06:00 +0100

    barcode generator initial code

---
+++ nuxt.config.js
@@ -0,0 +1,98 @@
+import dotenv from 'dotenv'
+import pkg from './package'
+
+dotenv.config()
+
+export default {
+  mode: 'universal',
+  router: {
+    linkExactActiveClass: 'active'
+    // linkActiveClass: 'active'
+  },
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'BarCode Generator',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description },
+      { hid: 'og:site_name', name: 'og:site_name', content: 'Barcode Generator' },
+      { hid: 'apple-mobile-web-app-title', name: 'apple-mobile-web-app-title', content: 'Barcode Generator' },
+      { hid: 'og:title', name: 'og:title', content: 'Barcode Generator' },
+      { name: 'theme-color', content: '#000000' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Merriweather:400,700,900|Rubik:400,500,700' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    '~/assets/scss/animate.scss',
+    '~/assets/scss/main.scss'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    { src: '~/plugins/universal', ssr: true },
+    { src: '~/plugins/mixins', ssr: true },
+    { src: '~/plugins/front-only', ssr: false }
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/style-resources'
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+    baseURL: process.env.API_URL,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+      'Access-Control-Allow-Credentials': 'true'
+    }
+  },
+  styleResources: {
+    scss: './assets/scss/variables.scss'
+  },
+  env: {
+    ApiUrl: process.env.API_URL
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Barcode Generator
+      </h1>
+      <h2 class="subtitle">
+        Enter a text below and click on generate to generate a barcode
+      </h2>
+      <div class="text-input">
+        <input v-model.trim="barcodeValue" type="text">
+      </div>
+      <!-- <button>
+        Generate Barcode
+      </button> -->
+      <div class="barcode-img-container">
+        <barcode :value="barcodeValue">
+          No barcode generated
+        </barcode>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VueBarcode from 'vue-barcode'
+
+export default {
+  components: {
+    'barcode': VueBarcode
+  },
+  data() {
+    return {
+      barcodeValue: ''
+    }
+  },
+  methods: {
+    // generateBarCode() {
+    //   console.log('====>', this.codeText)
+    // }
+  }
+}
+</script>
+
+<style lang="scss" src="./index.scss" scoped />
